adbf8968c096741916fa26bc70000d6358684476
Christian Noel Reyes <cris@nms.ph>
2020-09-15 16:16:24 +0800

    validations and args

--- index.js
+++ index.js
@@ -1,2 +1,38 @@
 'use strict';
 
+const minimist = require('minimist'),
+      fs = require('fs'),
+      moment = require('moment');
+
+function log(...message){
+  console.log(moment().format(),":", ...message);
+}
+
+let args = minimist(process.argv.slice(2), {
+    default: {
+        port: 8080
+    },
+});
+
+if(args.file === undefined || args.file == ""){
+  log("usage:\nnode index.js --file=\"./db-config.json\"");
+  process.exit(1);
+}
+
+if(!fs.existsSync(args.file)){
+  log(args.file, "file not found.");
+}
+
+var json = {};
+try{
+  json = require(args.file);
+
+  // var rawData = fs.readFileSync(args.file, "utf8");
+  // json = JSON.parse(JSON.stringify(rawData));
+}catch(e){
+  log("malformed json file in", args.file);
+  log(e);
+  process.exit(1);
+}
+
+log(json.source.user);
